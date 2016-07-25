fab17e02554fc72a6ca8f7bdf95ae99abdb428b0
Tyler Cash <contact@tylercash.xyz>
2016-07-25 17:36:21 +1000

    Added more features and fixed bugs - Player group is now displayed after role select - Player's now light up green if within the limits of their role + randoms, yellow if hit limit and red if over limit for role/affiliation -Fixed bug where role wouldn't be removed once role selected e.g if player was selected as bodyguard they would remain in bodyguard role + whatever other role they were selected as.

--- checker-logic.js
+++ checker-logic.js
@@ -1,14 +1,17 @@
-var TOTAL_PLAYERS = 15;
+var PLAYER_LIMIT = 0;
 var RULESET = 0;
-//0 = ranked
+var TOWN_LIMIT = 0;
+var MAFIA_LIMIT = 0;
 var available_roles = [];
-var GROUPS = [];
+var groups = [];
 var claimed_roles = [];
+
 
 function createPlayer(player_number, roles) {
     var html =
         "<li id=\"" + player_number + "\" class=\"collection-item\">" +
-        "<p class=\"player-num\">" + player_number + "</p>" +
+        "<p class=\"player-num left\">" + player_number + "</p>" +
+        "<p class=\"player-group left\"></p>" +
         "<select id=\"selector" + player_number + "\" class=\"role-selector right\">" +
         "<option value=\"0\" disabled selected>Please select role</option>";
     for (var i = 0; i < roles.length; i++) {
@@ -26,7 +29,7 @@ function initialSetup() {
     setupGameMode(RULESET);
     possible_roles = getPossibleRoles();
 
-    for (var i = 1; i < TOTAL_PLAYERS + 1; i++) {
+    for (var i = 1; i < PLAYER_LIMIT + 1; i++) {
         var element = createPlayer(i, available_roles);
         $("#alive-players").append(element);
         $("#selector" + i).material_select();
@@ -36,34 +39,40 @@ function initialSetup() {
         switch (RULESET) {
             //Ranked ruleset
             case 0:
-                available_roles = ["Amnesiac", "Arsonist", "Blackmailer", "Bodyguard", "Consiglere", "Consort", "Disguiser",
+                available_roles = ["Amnesiac", "Arsonist", "Blackmailer", "Bodyguard", "Consigliere", "Consort", "Disguiser",
                     "Doctor", "Escort", "Executioner", "Forger", "Framer", "Godfather", "Investigator", "Jailor",
                     "Janitor", "Jester", "Lookout", "Mafioso", "Mayor", "Medium", "Retributionist", "Serial Killer",
                     "Sheriff", "Spy", "Survivor", "Transporter", "Vampire Hunter", "Vigilante", "Werewolf", "Witch"];
-                GROUPS = [roleValues("Jailor", 1, ["Jailor"]),
-                    roleValues("Town Investigative", 2, ["Investigator", "Lookout", "Sheriff", "Spy"]),
-                    roleValues("Town Killing", 2, ["Vampire Hunter", "Veteran", "Vigilante"]),
-                    roleValues("Town Protective", 1, ["Bodyguard", "Doctor"]),
-                    roleValues("Town Support", 1, ["Escort", "Medium", "Mayor", "Retributionist", "Transporter"]),
-                    roleValues("Godfather", 1, ["Godfather"]),
-                    roleValues("Mafioso", 1, ["Mafioso"]),
-                    roleValues("Random Mafia", 1, ["Disguiser", "Forger", "Framer", "Janitor", "Blackmailer", "Consigliere", "Consort"]),
-                    roleValues("Neutral Killing", 1, ["Arsonist", "Serial Killer", "Werewolf"]),
-                    roleValues("Neutral Evil", 1, ["Executioner", "Jester", "Witch"]),
-                    roleValues("Neutral Benign", 1, ["Amnesiac", "Survivor"]),
-                    //("anything" == all town roles) == true
-                    roleValues("Any", 1, ["anything"])];
+                groups = [
+                    roleValues("Jailor", 2, ["Jailor"]),
+                    roleValues("Town Investigative", 3, ["Investigator", "Lookout", "Sheriff", "Spy"], "Town"),
+                    roleValues("Town Killing", 3, ["Vampire Hunter", "Veteran", "Vigilante"], "Town"),
+                    roleValues("Town Protective", 2, ["Bodyguard", "Doctor"], "Town"),
+                    roleValues("Town Support", 3, ["Escort", "Medium", "Mayor", "Retributionist", "Transporter"], "Town"),
+                    roleValues("Godfather", 1, ["Godfather"], "Mafia"),
+                    roleValues("Mafioso", 1, ["Mafioso"], "Mafia"),
+                    roleValues("Random Mafia", 2, ["Disguiser", "Forger", "Framer", "Janitor", "Blackmailer", "Consigliere", "Consort"],
+                        "Mafia"),
+                    roleValues("Neutral Killing", 1, ["Arsonist", "Serial Killer", "Werewolf"], "Neutral"),
+                    roleValues("Neutral Evil", 1, ["Executioner", "Jester", "Witch"], "Neutral"),
+                    roleValues("Neutral Benign", 1, ["Amnesiac", "Survivor"], "Neutral")
+                ];
+                TOWN_LIMIT = 9;
+                MAFIA_LIMIT = 4;
+                PLAYER_LIMIT = 15;
                 break;
         }
 
     }
 
-    function roleValues(name, number, roles) {
+    function roleValues(name, number, roles, affiliation) {
         return {
             "name": name,
             "quantity_limit": number,
             "quantity": 0,
-            "roles": roles
+            "roles": roles,
+            "affiliation": affiliation,
+            "location_in_array": []
         };
     }
 }
@@ -73,14 +82,79 @@ $(document).ready(function () {
 });
 
 function checkRoleValidity() {
+    function removeColor(ele) {
+        $(ele).attr("class", "collection-item");
+    }
+
     for (var key in claimed_roles) {
-        for (var i = 0; i < GROUPS.length; i++) {
-            for (var k = 0; k < GROUPS[i].roles.length; k++) {
-                if (claimed_roles[key] === GROUPS[i].roles[k]) {
-                    GROUPS[i].quantity++;
-                    if (GROUPS[i].quantity >= GROUPS[i].quantity_limit) {
-                        alert("Warning, " + GROUPS[i].name + " limit reached");
+        for (var i = 0; i < groups.length; i++) {
+            for (var k = 0; k < groups[i].roles.length; k++) {
+                if (claimed_roles[key] == groups[i].roles[k]) {
+
+                    var key_not_present = true;
+                    for (var j = 0; j < groups[i].location_in_array.length; j++) {
+                        if (groups[i].location_in_array[j] == key) {
+                            key_not_present = false;
+                            break;
+                        }
                     }
+
+                    if (key_not_present) {
+                        //If true then role is already in use elsewhere
+                        if (claimed_roles[key] != undefined) {
+                            //Finds and remove the other role that already exists
+                            for (var j = 0; j < groups.length; j++) {
+                                for (var l = 0; l < groups[j].location_in_array.length; l++) {
+                                    if (groups[j].location_in_array[l] == key) {
+                                        groups[j].location_in_array.splice(l, 1);
+                                    }
+                                }
+                            }
+                        }
+                        groups[i].location_in_array.push(key);
+                    }
+                }
+            }
+        }
+    }
+
+    for (var key in claimed_roles) {
+        for (var i = 0; i < groups.length; i++) {
+            for (var k = 0; k < groups[i].roles.length; k++) {
+                //If roles are over individual group cap.
+                var limitedGroup = 0;
+                if (groups[i].location_in_array.length === groups[i].quantity_limit) {
+                    limitedGroup = 1;
+                } else if (groups[i].location_in_array.length > groups[i].quantity_limit) {
+                    limitedGroup = 2;
+                }
+                //If roles are over town cap
+                if (groups[i].location_in_array.length === TOWN_LIMIT && groups[i].affiliation == "Town") {
+                    limitedGroup = 1;
+                } else if (groups[i].location_in_array.length > TOWN_LIMIT && groups[i].affiliation == "Town") {
+                    limitedGroup = 2;
+                }
+                //If roles are over mafia cap
+                if (groups[i].location_in_array.length === MAFIA_LIMIT && groups[i].affiliation == "Mafia") {
+                    limitedGroup = 1;
+                } else if (groups[i].location_in_array.length > MAFIA_LIMIT && groups[i].affiliation == "Mafia") {
+                    limitedGroup = 2;
+                }
+
+                for (var j = 0; j < groups[i].location_in_array.length; j++) {
+                    var current_element = groups[i].location_in_array[j];
+                    if (limitedGroup === 0) {
+                        removeColor("#" + current_element);
+                        $("#" + current_element).addClass("#b9f6ca green accent-1");
+                    }
+                    if (limitedGroup === 1) {
+                        removeColor("#" + current_element);
+                        $("#" + current_element).addClass("#ffecb3 amber lighten-4");
+                    } else if (limitedGroup === 2) {
+                        removeColor("#" + current_element);
+                        $("#" + current_element).addClass("#ffcdd2 red lighten-4");
+                    }
+                    $("#" + current_element).children(".player-group").html(groups[i].name);
                 }
             }
         }
